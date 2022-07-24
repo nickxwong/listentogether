1855894bfda26dfbb0662505933cd631917f7858
Nick Wong <nicolaswong01@gmail.com>
2022-07-24 15:40:31 -0700

    Finish functionality to create a shared playlist

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const client_id = 'de95ef31db374610aa1ebd2910a8c3c8';
 const client_secret = 'c56cf730a6f442ee8fd38ce757721588'
 const redirect_uri = 'http://127.0.0.1:5500/index.html';
-let user_library = new Set();
+let library_a = new Set();
+let shared_library = new Set();
 
 function requestAuthorization() {
     let url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURI(redirect_uri)}&show_dialog=true&scope=playlist-modify-public user-library-read`;
@@ -41,7 +42,7 @@ function getAccessToken(code) {
 function handleAuthorizationResponse() {
     if (this.status == 200) {
         let data = JSON.parse(this.responseText);
-        if (sessionStorage.getItem("access_a") == null) { // first account connection
+        if (sessionStorage.getItem('access_a') == null) { // first account connection
             sessionStorage.setItem('access_a', data.access_token);
             getProfileInfo('a');
         } else { // second account connection
@@ -62,7 +63,11 @@ function getProfileInfo(user_id) {
 function callAPI(method, endpoint, request, callback, user_id) {
     const xhr = new XMLHttpRequest();
     xhr.open(method, endpoint, true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('access_a'));
+    if (user_id == 'a') {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('access_a'));    
+    } else { // user_id == 'b'
+        xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('access_b'));    
+    }
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(request);
     xhr.onload = (e) => {
@@ -78,7 +83,7 @@ function callAPI(method, endpoint, request, callback, user_id) {
 }
 
 function saveProfileInfo(user_id, user_data) {
-    if (sessionStorage.getItem("display_name_a") == null) { // first account connection
+    if (sessionStorage.getItem('display_name_a') == null) { // first account connection
         sessionStorage.setItem('display_name_a', user_data.display_name);
         sessionStorage.setItem('profile_pic_a', user_data.images[0].url);
     } else { // second account connection
@@ -90,14 +95,14 @@ function saveProfileInfo(user_id, user_data) {
 
 function populateProfileInfo() {
     // populate profile for user a
-    if (sessionStorage.getItem("access_a") != null) {
+    if (sessionStorage.getItem('access_a') != null) {
         let display_name = document.querySelector('.user#a .header .display-name');
         let profile_pic = document.querySelector('.user#a .profile-pic img');
         display_name.innerHTML = `User: ${sessionStorage.getItem('display_name_a')}`;
         profile_pic.setAttribute('src', sessionStorage.getItem('profile_pic_a'));
     }
     // populate profile for user b
-    if (sessionStorage.getItem("access_b") != null) {
+    if (sessionStorage.getItem('access_b') != null) {
         let display_name = document.querySelector('.user#b .header .display-name');
         let profile_pic = document.querySelector('.user#b .profile-pic img');
         display_name.innerHTML = `User: ${sessionStorage.getItem('display_name_b')}`;
@@ -105,23 +110,42 @@ function populateProfileInfo() {
     }
 }
 
-function retrieveLibrary() {
-    let request = 'market=US&limit=50&offset=0';
-    callAPI('GET', 'https://api.spotify.com/v1/me/tracks', request, saveSongs);
-}
-
-function saveSongs(user_id) {
-    if (this.status == 200) {
-        let song_list = JSON.parse(this.responseText).items;
-        song_list.forEach((song, i) => {
-            user_library.add(song.track.name + ' - ' + song.track.artists[0].name);
-        })
-        // getProfileInfo();
+function createSharedPlaylist() {
+    if (sessionStorage.getItem('access_a') == null || sessionStorage.getItem('access_b') == null) {
+        alert("Please connect two accounts first.");
     } else {
-        alert("ERROR: Library retrieval failed");
-        console.log("Status: " + this.status);
-        console.log("Response: " + this.responseText);
+        retrieveLibrary('a');
     }
 }
 
+function retrieveLibrary(user_id) {
+    let request = 'market=US&limit=50&offset=0';
+    if (user_id == 'a') {
+        callAPI('GET', 'https://api.spotify.com/v1/me/tracks', request, saveSongs, user_id);    
+    } else { // user_id == 'b'
+        callAPI('GET', 'https://api.spotify.com/v1/me/tracks', request, getLibraryUnion, user_id);
+    }
+    
+}
 
+function saveSongs(user_id, user_data) {
+    let user_library = user_data.items;
+    user_library.forEach((song, i) => {
+        library_a.add(song.track.name + ' - ' + song.track.artists[0].name);
+    })
+    retrieveLibrary('b');
+}
+
+function getLibraryUnion(user_id, user_data) {
+    let user_library = user_data.items;
+    user_library.forEach((song, i) => {
+        let cur_song = song.track.name + ' - ' + song.track.artists[0].name;
+        if (library_a.has(cur_song)) {
+            shared_library.add(cur_song);
+        }
+    })
+    shared_library.forEach((song, i) => {
+        console.log(song);
+    })
+    
+}
